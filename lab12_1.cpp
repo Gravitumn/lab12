5a30b4616134db734b4e632a608ba5b63f0082db
leekyungwoo <thanisorn_c@cmu.ac.th>
2021-01-19 16:22:13 +0700

    555555555555

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,33 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum=0;
+    for(int i=0;i<N;i++){
+        sum+=A[i];
+    }
+    B[0]=sum/N;
+    double sum1=0;
+    for(int i=0;i<N;i++){
+        sum1+=pow(A[i]-B[0],2);
+    }
+    B[1]=sqrt(sum1/N);
+    double sum2=1;
+    for(int i=0;i<N;i++){
+        sum2*=A[i];
+    }
+    B[2]=pow(sum2,1.0/N);
+    double sum3=0;
+    for(int i=0;i<N;i++){
+        sum3+=1.0/A[i];
+    }
+    B[3]=N/sum3;
+    double max=B[0],min=B[0];
+    for(int i=0;i<N;i++){
+        if(max<A[i])max=A[i];
+        else if(min>A[i])min=A[i];
+    }
+    B[4]=max;
+    B[5]=min;
+}
